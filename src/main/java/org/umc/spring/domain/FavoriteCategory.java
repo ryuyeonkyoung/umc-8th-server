dc854942014105a97a514683c56085ed55382e5a
류연경 <ykr.dev@gmail.com>
2025-04-21 16:58:21 +0900

    chore: mission-05 제약 조건 설정 (@Column,@Lob)

--- src/main/java/org/umc/spring/domain/FavoriteCategory.java
+++ src/main/java/org/umc/spring/domain/FavoriteCategory.java
@@ -14,5 +14,6 @@ public class FavoriteCategory {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 50)
     private String name;
 }
--- src/main/java/org/umc/spring/domain/Region.java
+++ src/main/java/org/umc/spring/domain/Region.java
@@ -14,5 +14,6 @@ public class Region {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 50)
     private String name;
 }
