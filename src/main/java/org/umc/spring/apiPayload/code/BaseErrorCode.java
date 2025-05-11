6b1101063fa659e793b0898c9db22b77df08cd33
류연경 <ykr.dev@gmail.com>
2025-05-11 15:46:33 +0900

    feat: practice-07 표준화된 API 응답 및 예외 처리 로직 구현
    - `ApiResponse` 클래스 추가로 성공 및 실패 응답 표준화.
    - `SuccessStatus`, `ErrorStatus` Enum을 활용한 상태 코드 및 메시지 관리.
    - `GeneralException`, `TempHandler`를 통한 커스텀 예외 처리 구조 설계.
    - `ExceptionAdvice`를 활용한 전역 예외 처리 및 응답 일관성 확보.
    - 테스트용 컨트롤러 및 서비스(`TempRestController`, `TempQueryService`) 구현.

---
+++ src/main/java/org/umc/spring/apiPayload/code/BaseErrorCode.java
@@ -0,0 +1,8 @@
+package org.umc.spring.apiPayload.code;
+
+public interface BaseErrorCode {
+
+    ErrorReasonDTO getReason();
+
+    ErrorReasonDTO getReasonHttpStatus();
+}
---
+++ src/main/java/org/umc/spring/service/TempService/TempCommandService.java
@@ -0,0 +1,5 @@
+package org.umc.spring.service.TempService;
+
+public interface TempCommandService {
+    void CheckFlag(Integer flag);
+}
