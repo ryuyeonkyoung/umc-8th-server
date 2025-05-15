2149647d9f1ecd8395c00d872504c8d51e713ce3
류연경 <ykr.dev@gmail.com>
2025-05-15 20:27:21 +0900

    feature: practice-08 커스텀 Validator 어노테이션 도입(@ExistCategories) #1
    - `@ExistCategories` 어노테이션 정의 (validatedBy = CategoriesExistValidator)
    - `CategoriesExistValidator` 구현: `ConstraintValidator<ExistCategories, List<Long>>` 적용, DB 존재 여부 검증 로직 추가
    - DTO 필드에 `@ExistCategories` 선언 및 Controller 파라미터에 `@Valid` 적용하여 자동 검증 트리거

--- src/main/java/org/umc/spring/dto/MemberRequestDTO.java
+++ src/main/java/org/umc/spring/dto/MemberRequestDTO.java
@@ -1,6 +1,10 @@
 package org.umc.spring.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import org.umc.spring.validation.annotation.ExistCategories;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,17 +13,29 @@ public class MemberRequestDTO {
 
     @Getter
     public static class JoinDto {
+        @NotBlank
         private String name;
+        @NotNull
         private String nickname;
+        @NotNull
         private String socialId;
+        @NotNull
         private Integer socialType; // 'kakao', 'naver', 'google'
+        @NotNull
         private String phoneNumber;
+        @NotNull
         private Boolean phoneVerified;
+        @NotNull
         private Integer gender; // 'male', "female", "none"
+        @Size(min = 5, max = 12)
         private String address;
+        @NotNull
         private String email;
+        @NotNull
         private Integer point;
+        @NotNull
         private Integer status; // "active", "inactive", "suspended"
+        @ExistCategories
         private List<Long> preferCategory = new ArrayList<>();
     }
 }
