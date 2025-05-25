44186694290b737a6a9531a5a4aa68f29e8832b9
류연경 <ykr.dev@gmail.com>
2025-05-25 15:36:28 +0900

    feat: practice-09 가게 리뷰 목록 조회 API 구현 #12
    - Swagger를 통한 API 명세 작성
    - Page를 사용해 페이징 구현

--- src/main/java/org/umc/spring/converter/StoreConverter.java
+++ src/main/java/org/umc/spring/converter/StoreConverter.java
@@ -1,8 +1,13 @@
 package org.umc.spring.converter;
 
+import org.springframework.data.domain.Page;
+import org.umc.spring.domain.Review;
 import org.umc.spring.domain.Store;
 import org.umc.spring.dto.store.request.StoreRequestDTO;
 import org.umc.spring.dto.store.response.StoreResponseDTO;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class StoreConverter {
     public static StoreResponseDTO.CreateResultDto toCreateResultDTO(Store store) {
@@ -24,6 +29,29 @@ public class StoreConverter {
                 .closedDay(request.getClosedDay())
                 .openTime(request.getOpenTime())
                 .closeTime(request.getCloseTime())
+                .build();
+    }
+
+    public static StoreResponseDTO.ReviewPreViewDTO reviewPreViewDTO(Review review){
+        return StoreResponseDTO.ReviewPreViewDTO.builder()
+                .ownerNickname(review.getMember().getName()) // 객체 그래프 탐색
+                .score(review.getRating())
+                .createdAt(review.getCreatedAt().toLocalDate())
+                .body(review.getContext())
+                .build();
+    }
+    public static StoreResponseDTO.ReviewPreViewListDTO reviewPreViewListDTO(Page<Review> reviewList){
+
+        List<StoreResponseDTO.ReviewPreViewDTO> reviewPreViewDTOList = reviewList.stream()
+                .map(StoreConverter::reviewPreViewDTO).collect(Collectors.toList());
+
+        return StoreResponseDTO.ReviewPreViewListDTO.builder()
+                .isLast(reviewList.isLast())
+                .isFirst(reviewList.isFirst())
+                .totalPage(reviewList.getTotalPages())
+                .totalElements(reviewList.getTotalElements())
+                .listSize(reviewPreViewDTOList.size())
+                .reviewList(reviewPreViewDTOList)
                 .build();
     }
 }
--- src/main/java/org/umc/spring/repository/ReviewRepository/ReviewRepository.java
+++ src/main/java/org/umc/spring/repository/ReviewRepository/ReviewRepository.java
@@ -1,7 +1,11 @@
 package org.umc.spring.repository.ReviewRepository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.umc.spring.domain.Review;
+import org.umc.spring.domain.Store;
 
 public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom {
+    Page<Review> findAllByStore(Store store, PageRequest pageRequest);
 }
