ec58ccd309147e4f9ccbf0ecbd10295677ffc976
류연경 <ykr.dev@gmail.com>
2025-05-25 17:24:32 +0900

    chore: DTO 클래스명 통일(Dto → DTO)

--- src/main/java/org/umc/spring/repository/MemberRepository/MemberRepositoryCustom.java
+++ src/main/java/org/umc/spring/repository/MemberRepository/MemberRepositoryCustom.java
@@ -2,11 +2,11 @@ package org.umc.spring.repository.MemberRepository;
 
 import org.springframework.data.domain.Slice;
 import org.umc.spring.domain.enums.MissionStatus;
-import org.umc.spring.dto.mission.response.CursorPagedMissionResponseDto;
-import org.umc.spring.dto.member.response.MemberProfileResponseDto;
+import org.umc.spring.dto.mission.response.CursorPagedMissionResponseDTO;
+import org.umc.spring.dto.member.response.MemberProfileResponseDTO;
 
 public interface MemberRepositoryCustom {
-    MemberProfileResponseDto findMemberProfileById(Long memberId);
-    Slice<CursorPagedMissionResponseDto> findCompletedMissionsByCursor(Long memberId, Long lastMissionId, MissionStatus memberMissionStatus);
+    MemberProfileResponseDTO findMemberProfileById(Long memberId);
+    Slice<CursorPagedMissionResponseDTO> findCompletedMissionsByCursor(Long memberId, Long lastMissionId, MissionStatus memberMissionStatus);
 
 }
