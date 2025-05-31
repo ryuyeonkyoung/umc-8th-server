60a24777097f9d088cabeae788c5e90fa1e5443c
류연경 <104209685+ryuyeonkyoung@users.noreply.github.com>
2025-05-31 18:47:29 +0900

    feat: mission-10 JWT 기반 회원가입, 로그인, 내 정보 조회 API 구현 (Spring Security) #25

--- src/main/java/org/umc/spring/service/MemberService/MemberQueryService.java
+++ src/main/java/org/umc/spring/service/MemberService/MemberQueryService.java
@@ -1,11 +1,14 @@
 package org.umc.spring.service.MemberService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.data.domain.Slice;
 import org.umc.spring.domain.enums.MissionStatus;
 import org.umc.spring.dto.member.response.MemberProfileResponseDTO;
+import org.umc.spring.dto.member.response.MemberResponseDTO;
 import org.umc.spring.dto.mission.response.CursorPagedMissionResponseDTO;
 
 public interface MemberQueryService {
     MemberProfileResponseDTO loadMemberProfile(Long memberId);
     Slice<CursorPagedMissionResponseDTO> loadCompletedMissions(Long memberId, Long lastMissionId, MissionStatus missionStatus);
+    MemberResponseDTO.MemberInfoDTO getMemberInfo(HttpServletRequest request);
 }
